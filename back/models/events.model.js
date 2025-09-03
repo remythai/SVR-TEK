3e3407006ebbacdfa9cacab2cfbec0010ba2c949
remythai <remy.thai@epitech.eu>
2025-09-03 16:26:01 +0200

    feat: deletion for the events

--- back/models/events.model.js
+++ back/models/events.model.js
@@ -9,5 +9,13 @@ export async function getById(sql, id) {
 export async function getEventImage(sql, eventsId) {
     return await sql`SELECT image FROM events WHERE id = ${eventsId}`;
 }
+
+// ------------
+// -- Delete --
+// ------------
+
+export async function deleteById(sql, id) {
+    return await sql`DELETE FROM events WHERE id = ${id}`;
+}
   
-export default { getAll, getById, getEventImage };
+export default { getAll, getById, getEventImage, deleteById };
--- back/routes/events.routes.js
+++ back/routes/events.routes.js
@@ -7,4 +7,6 @@ router.get("/", eventsController.getAll);
 router.get("/:id", eventsController.getById);
 router.get("/:id/image", eventsController.getEventImage);
 
+router.delete("/:id", eventsController.deleteById);
+
 export default router;
