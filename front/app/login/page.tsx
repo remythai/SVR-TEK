b20a1a7cdba571655dc6dcb77be901f46a882127
Simon Maigrot <simon.maigrot33@gmail.com>
2025-09-03 09:14:00 +0200

    feat: Separating register and login page

--- front/app/login/page.tsx
+++ front/app/login/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <>
       <div className="flex min-h-[70vh] flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Register a new account</h2>
+          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Login to your account</h2>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
@@ -47,7 +47,7 @@ export default function Home() {
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
               >
-                Register
+                Login
               </button>
             </div>
           </form>
