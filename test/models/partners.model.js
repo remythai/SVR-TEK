63bcbe86704245581967cef7cda6a6c139efd840
Antton Ducos <antton.ducos@epitech.eu>
2025-09-03 14:26:24 +0200

    delete: delete all useless files

--- test/models/partners.model.js
+++
@@ -1,9 +0,0 @@
-export async function getAll(sql) {
-    return await sql`SELECT * FROM partners`;
-}
-  
-export async function getById(sql, id) {
-    return await sql`SELECT * FROM partners WHERE id = ${id}`;
-}
-  
-export default { getAll, getById };
--- test/routes/partners.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import * as partnersController from "../controllers/partners.controller.js"
-
-const router = express.Router();
-
-router.get("/", partnersController.getAll);
-router.get("/:id", partnersController.getById);
-
-export default router;
