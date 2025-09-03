837467bb279ffcc01ec961ab42a9086fdfc14f21
Antton Ducos <antton.ducos@epitech.eu>
2025-09-03 16:54:40 +0200

    refacto & feat: add create function to investors and refacto to use utils functions

--- back/models/investors.model.js
+++ back/models/investors.model.js
@@ -1,4 +1,5 @@
 export async function getAll(sql) {
+<<<<<<< HEAD
     return await sql`SELECT * FROM investors`;
   }
   
@@ -11,3 +12,25 @@ export async function getAll(sql) {
   }
   
   export default { getAll, getById, getInvestorImage };
+=======
+  return await dbUtils.getAll(sql, TABLE_NAME);
+}
+
+export async function getById(sql, id) {
+  return await dbUtils.getById(sql, TABLE_NAME, id);
+}
+
+export async function getInvestorImage(sql, newsId) {
+  return (await dbUtils.getImageById(sql, TABLE_NAME, newsId))
+}
+
+// ------------
+// -- Create --
+// ------------
+
+export async function create(sql, data) {
+  return await dbUtils.create(sql, TABLE_NAME, data);
+}
+
+export default { getAll, getById, getInvestorImage, create };
+>>>>>>> 955c4e9 (refacto & feat: add create function to investors and refacto to use utils functions)
--- back/routes/investors.routes.js
+++ back/routes/investors.routes.js
@@ -3,8 +3,18 @@ import * as investorsController from "../controllers/investors.controller.js"
 
 const router = express.Router();
 
+// ----------
+// -- Read --
+// ----------
+
 router.get("/", investorsController.getAll);
 router.get("/:id", investorsController.getById);
 router.get("/:id/image", investorsController.getInvestorImage);
+
+// ------------
+// -- Create --
+// ------------
+
+router.post("/", investorsController.create);
 
 export default router;
