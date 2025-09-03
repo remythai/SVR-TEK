846b9687af6d565e784d1d28f166760d36524c5d
Louka Ortega Cand <louka.ortega-cand@epitech.eu>
2025-09-03 03:41:43 +0200

    fix: change Register to a new account into login to your account in login page

--- front/app/login/page.tsx
+++ front/app/login/page.tsx
@@ -52,7 +52,7 @@ export default function Login() {
     <>
       <div className="flex min-h-[70vh] flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Register a new account</h2>
+          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Login to your account</h2>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
