8b682d1af6c32fa132425ef9a7f83cbe26f92a2f
Simon Maigrot <simon.maigrot33@gmail.com>
2025-09-05 10:27:30 +0200

    fix: Fixing login and register form style

--- front/app/login/page.tsx
+++ front/app/login/page.tsx
@@ -52,7 +52,7 @@ export default function Login() {
     <>
       <div className="flex min-h-[70vh] flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Login to your account</h2>
+          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight">Login to your account</h2>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
@@ -67,7 +67,7 @@ export default function Login() {
                     <FormItem>
                     <FormLabel>Email</FormLabel>
                     <FormControl>
-                        <Input placeholder="shadcn" {...field} />
+                        <Input placeholder="Ex: QuentinLePlusSexy@gmail.com" {...field} />
                     </FormControl>
                     <FormMessage />
                     </FormItem>
@@ -95,7 +95,7 @@ export default function Login() {
               <button
                 type="submit"
                 disabled={isLoading}
-                className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                className="flex w-full justify-center rounded-md bg-primary-200 hover:bg-primary-300 transition-colors duration-300 px-3 py-1.5 text-sm/6 font-semibold text-white cursor-pointer"
               >
                 {isLoading ? "..." : "Login"}
               </button>
